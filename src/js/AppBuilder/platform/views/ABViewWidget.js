4afa5209877714618828a8d75cfa09c691bfa8f6
Johnny Hausman <johnny@hausmanfamily.com>
2023-06-02 14:40:47 +0700

    [add] missing views 3

---
+++ src/js/AppBuilder/platform/views/ABViewWidget.js
@@ -0,0 +1,29 @@
+import ABViewWidgetCore from "../../core/views/ABViewWidgetCore";
+
+const ABPropertyComponentDefaults = ABViewWidgetCore.defaultValues();
+
+export default class ABViewWidget extends ABViewWidgetCore {
+   // constructor(values, application, parent, defaultValues) {
+   //    super(values, application, parent, defaultValues);
+   // }
+
+   /**
+    * @function component()
+    * return a UI component based upon this view.
+    * @param {obj} App
+    * @return {obj} UI component
+    */
+   component(App) {
+      let base = super.component(App);
+
+      base.onShow = (viewId) => {
+         let dv = this.datacollection; // get from a function or a (get) property
+         if (dv && dv.dataStatus == dv.dataStatusFlag.notInitial) {
+            // load data when a widget is showing
+            dv.loadData();
+         }
+      };
+
+      return base;
+   }
+}
