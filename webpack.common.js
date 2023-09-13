80639fb046aee8c1dd84e82e7a92c9d03508eb7b
nohrsom <sky_wyvern@hotmail.com>
2023-09-13 12:12:50 +0700

    Remove hash on compiled file's names

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
    },
    output: {
       path: path.join(APP, "..", "web", "assets", "mobile"),
-      filename: "mobile_[name].[contenthash].js",
+      filename: "mobile_[name].js",
    },
    module: {
       rules: [
