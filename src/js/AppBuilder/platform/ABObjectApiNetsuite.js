0ad1ca36ae40db049981a03f93c4471cd6008168
Johnny <johnny@digiserve.org>
2025-03-19 17:51:25 -0500

    [sync] with core

---
+++ src/js/AppBuilder/platform/ABObjectApiNetsuite.js
@@ -0,0 +1,10 @@
+import ABObjectApiNetsuiteCore from "../core/ABObjectApiNetsuiteCore";
+
+/**
+ * ABObjectApiNetsuite
+ * These are ABObjects that were Imported from existing Tables in the
+ * system.
+ *
+ * NOTE: on the web client, these should function just like ABObjects
+ */
+export default class ABObjectApiNetsuite extends ABObjectApiNetsuiteCore {}
